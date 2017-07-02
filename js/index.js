2f910acf3e592f5ce7ea8ee013b5ec878afd25c6
Noah Weiner <noahbweiner@gmail.com>
2017-07-02 10:24:06 -0700

    I FORGOT TO ADD YEARS

--- js/index.js
+++ js/index.js
@@ -2,15 +2,20 @@
 
 var shutdown = moment("2019-07-01T00:00:00-07:00");
 
+var format = function (num, str) {
+  return num > 0 ? '<b>' + num + ' ' + str + '</b>, ' : '';
+}
+
 var update = function () {
   var duration = moment.duration(shutdown.diff(moment()));
 
   if (duration > 0) {
     var string =
-      '<b>' + duration.days() + ' days</b>, ' +
-      '<b>' + duration.hours() + ' hours</b>, ' +
-      '<b>' + duration.minutes() + ' minutes</b>, ' +
-      '<b>' + duration.seconds() + ' seconds</b>, ' +
+      format(duration.years(), 'years') + 
+      format(duration.days(), 'days') + 
+      format(duration.hours(), 'hours') +
+      format(duration.minutes(), 'minutes') +
+      format(duration.seconds(), 'seconds') +
       'and <b>' + duration.milliseconds() + ' milliseconds</b>';
 
     document.getElementById('countdown').innerHTML = string;
